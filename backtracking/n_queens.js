069c48b0fb8e2dbdd01ca397cb39c97bf6161c78
phuhung273 <phuhung.tranpham@gmail.com>
2022-05-02 23:43:13 +0700

    add backtracking n_queens

---
+++ backtracking/n_queens.js
@@ -0,0 +1,96 @@
+/**
+ * @param  {Array} state
+ * @param  {Number} n
+ * @return {boolean}
+ */
+function isValidState(state, n) {
+  return state.length === n;
+}
+
+/**
+ * @param  {Array} state
+ * @param  {Number} number
+ * @return {Array}
+ */
+function getCandidates(state, number){
+  const candidates = [];
+  const y = state.length;
+
+  for (let x = 0; x < number; x++) {
+    if (!state.includes(x)) {
+      // Not the same column
+
+      if (y === 0) {
+        candidates.push(x);
+      } else {
+
+        let notDiagonal = true;
+        for (let yState = 0; yState < y; yState++) {
+          xState = state[yState];
+          // console.log(yState + " " + xState + " " + y + " " + x);
+          if (Math.abs(y - yState) === Math.abs(x - xState)) {
+            notDiagonal = false;
+            break;
+          }
+        }
+
+        if (notDiagonal) {
+          // console.log('Found ' + x);
+          candidates.push(x);
+        }
+      }
+
+    }
+  }
+
+  return candidates;
+}
+
+/**
+ * @param  {Array} state
+ * @param  {Array} solutions
+ * @param  {Number} solutions
+ */
+function search(state, solutions, number){
+  if (isValidState(state, number)) {
+    solutions.push(state);
+    // console.log(solutions);
+    // // If only require 1 solution, return immediately
+    // return;
+  }
+
+  for(let candidate of getCandidates(state, number)){
+    // console.log(candidate);
+    state.push(candidate);
+    // console.log(`Before search ${state}`);
+    search(state.slice(), solutions, number);
+    // console.log(`Finish candidate ${candidate} for state: ${state}`);
+    state = state.filter(e => e !== candidate);
+    // console.log(`After search ${state}`);
+  }
+
+  // console.log(`Finish all state ${state}`);
+}
+/**
+ * @param  {Number} number
+ */
+function solve(number){
+  solutions = [];
+  state = [];
+  search(state, solutions, number);
+  return solutions;
+}
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Type n: ', number => {
+  number = parseInt(number);
+
+  console.time('test');
+  console.log(solve(number));
+  console.timeEnd('test');
+  readline.close();
+});
---
+++ backtracking/template.js
@@ -0,0 +1,45 @@
+// https://www.youtube.com/watch?v=A80YzvNwqXA
+
+/**
+ * @param  {any} state
+ * @return {boolean}
+ */
+function isValidState(state) {
+  return true;
+}
+
+/**
+ * @param  {any} state
+ * @return {Array}
+ */
+function getCandidates(state){
+  return [];
+}
+
+/**
+ * @param  {any} state
+ * @param  {Array} solutions
+ */
+function search(state, solutions){
+  if (isValidState(state)){
+    solutions.push(state);
+    console.log(solutions);
+    // // If only require 1 solution, return immediately
+    // return;
+  }
+
+  for(let candidate of getCandidates(state)){
+    // console.log(candidate);
+    state.push(candidate);
+    // console.log(state);
+    search(state, solutions)
+    state = state.filter(e => false);
+  }
+}
+
+function solve(){
+  solutions = [];
+  state = [];
+  search(state, solutions);
+  return solutions;
+}
